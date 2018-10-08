a30582a5441d6894c52368dfe5c032ea9aa626bb
PineREN <35160614+PineREN@users.noreply.github.com>
2018-10-08 14:45:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pine/helloworld/helloworldActivity.java
@@ -0,0 +1,13 @@
+package com.example.pine.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helloworldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_latout);
+    }
+}
